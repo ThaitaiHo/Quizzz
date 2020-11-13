0c7829ea4e04f405f65de22763c92c94fe884d7e
ThaitaiHo <42505669+ThaitaiHo@users.noreply.github.com>
2020-11-13 12:58:09 +0700

    11

--- Quizzz/app/src/main/java/com/example/quizzz/Playing.java
+++
@@ -1,6 +0,0 @@
-package com.example.quizzz;
-
-import android.app.Activity;
-
-public class Playing extends Activity {
-}
--- Quizzz/app/src/main/java/com/example/quizzz/QuestionSetup.java
+++
@@ -1,4 +0,0 @@
-package com.example.quizzz;
-
-public class QuestionSetup {
-}
