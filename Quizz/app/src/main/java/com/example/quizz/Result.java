c7f1390356a3283d481f2ac4b1bc636940a00811
trongpham992k-gif <73296940+trongpham992k-gif@users.noreply.github.com>
2020-11-14 01:02:03 +0700

    1:01 11.14.2020

--- Quizz/app/src/main/java/com/example/quizz/Result.java
+++ Quizz/app/src/main/java/com/example/quizz/Result.java
@@ -1,6 +1,27 @@
 package com.example.quizz;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Result extends Activity {
+    Button back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+
+        back = (Button)findViewById(R.id.back_btn);
+
+        //chuyen ve trang level
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Result.this, Level.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
